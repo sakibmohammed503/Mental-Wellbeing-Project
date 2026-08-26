51f7e50d6d5bc064608c110309e34ab3fc0f704e
sakibmohammed503 <your-github-email@example.com>
2026-08-26 14:40:18 +0600

    Add requirements for Render Deployment

--- backend/app.py
+++ backend/app.py
@@ -107,9 +107,5 @@ def home():
 
 if __name__ == "__main__":
     ensure_excel_exists()
-    app.run(debug=True, port=5000)
-
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
